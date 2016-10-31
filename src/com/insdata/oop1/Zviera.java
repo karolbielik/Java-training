3df2190016b0a37cffa8300da0503dbf7e2f91bc
karol.bielik <karol.bielik@gmail.com>
2016-10-31 08:53:28 +0100

    oop1

---
+++ src/com/insdata/oop1/Zviera.java
@@ -0,0 +1,27 @@
+package com.insdata.oop1;
+
+/**
+ * Created by karol.bielik on 27. 10. 2016.
+ */
+
+//Triery(Objekty) Sa Pisu Velkym Zaciatocnym Pismenom  A Z Pravidla Su Podstatne Meno
+/*public*/ class Zviera {
+
+    //konstruktor triedy
+    Zviera(){
+        //zradlo = "zviera zerie zradlo";
+    }
+
+    {
+        zradlo = "zviera zerie zradlo";
+    }
+
+    //dobrym zvykom je drzat premonne triedy ako privat a zpristupnit ich cez motodu kvoli enkapsulacii
+    //nieje to vsak povinnost
+    String zradlo;
+
+    //funkcieSaPisuMalymZaciatocnymPismenomADalejCamelCase
+    String zerie(){
+        return zradlo;
+    }
+}
