8d14d4ffe2bdbc4b1da0c803a39ba661279fe37e
karolbielik <karol.bielik@gmail.com>
2018-04-25 16:15:25 +0200

    multiple inheritance

--- src/com/insdata/defaultmethod/DefaultMethod.java
+++ src/com/insdata/defaultmethod/DefaultMethod.java
@@ -1,7 +1,5 @@
 package com.insdata.defaultmethod;
 
-import java.util.Objects;
-
 public class DefaultMethod {
     public static void main(String[] args) {
 
@@ -21,7 +19,7 @@ public class DefaultMethod {
 interface TestDefault{
     String mojeMenoJe();
     //default keyword bolo do java uvedene kvoli spetnej kompatibilite
-    //napr. v interfac Iterable bola do java 8 uvedena metoda forEach
+    //napr. v interface Iterable bola do java 8 uvedena metoda forEach
     //kedze Collection rozsiruje Iterable, tak by vsetky implementacie
     //Collection museli implementovat forEach. Bez toho aby sa narusila
     //implementacia vsetkych Collection interface, tak sa musela uviest do interface
